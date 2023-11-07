aeb31fa78c56d0318cb9fbe7f522447a46a894e0
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-11-06 18:32:11 -0800

    all

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        row_len = len(row)
+        for col in range(row_len):
+            if col != row_len - 1:
+                print("{:d}".format(row[col]), end=' ')
+            else:
+                print("{:d}".format(row[col]), end='')
+        print()
