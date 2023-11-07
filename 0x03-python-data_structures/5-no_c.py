877d3ee77f6601bc135f56a188a5d1afcaca214a
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-11-07 13:29:14 -0800

    all

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = len(my_string).remove("c","C")
-    print( new_str)
+    len(my_string).remove("c","C")
+    new_str = my_string.copy()
+    print(new_str)
