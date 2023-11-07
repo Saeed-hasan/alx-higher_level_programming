a8fe71060f8a79efbea87280036fda21052aacd4
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-11-06 19:02:00 -0800

    all

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    s-length = len(sentence)
+    if s-length == 0:
+        first character = None
+    else:
+        first character = sentence[0]
+    return ((s-length, first character))
