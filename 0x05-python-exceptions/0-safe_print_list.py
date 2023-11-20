a41326f8894e4122bdd855458ac6de68de5e5d22
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-11-20 15:05:42 -0800

    all

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """
+    prints a list of anything, but only prints the integers
+    Returns the amount of integers printed
+    """
+    result = 0
+    for i in range(x):
+        try:
+            print(f"my_list=[i]", end = "")
+            result += 1
+        except:
+            break
+    print()
+    return result
