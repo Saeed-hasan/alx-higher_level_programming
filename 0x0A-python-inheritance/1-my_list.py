e26a7cf2e5d9138fab5f64c8676e7398d6121f8f
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-12-28 04:11:17 -0800

    all

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,10 +11,6 @@ contain public method that sort list
 class MyList(list):
     """ inherts from a list"""
 
-    def __init__(self):
-        """ initializes the object """
-        super().__init__()
-
     def print_sorted(self):
         """ print the the list but sorted """
         sorted_list = sorted(self)
