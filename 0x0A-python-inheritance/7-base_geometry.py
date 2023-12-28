927466f2a78c664e200d0564b2eb22701e7f5a60
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-12-28 05:51:40 -0800

    all

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,6 +15,5 @@ class BaseGeometry:
         """ check validate of integer """
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-        if  value <= 0:
+        if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
-
