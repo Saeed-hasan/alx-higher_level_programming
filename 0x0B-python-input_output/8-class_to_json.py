dcdd3d183c5e8707df8d26cbba99f65b99eba45b
Saeed-hasan <saeedmohammed1999@gmail.com>
2024-01-03 07:24:52 -0800

    all

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines a Python class-to-JSON function."""
+import json
 
 
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    return json.dumps(obj.__dict__)
