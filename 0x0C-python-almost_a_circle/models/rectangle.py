601c8699df2fdd45f33c46356c4c2ad6e09441b7
Saeed-hasan <saeedmohammed1999@gmail.com>
2024-01-15 16:19:59 -0800

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -58,5 +58,7 @@ class Rectangle(Base):
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif value <= 0:
             raise ValueError("{} must be > 0".format(name))
