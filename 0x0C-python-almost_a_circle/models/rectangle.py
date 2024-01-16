6cac42ef736ecae7201784a2620733b1395a2957
Saeed-hasan <saeedmohammed1999@gmail.com>
2024-01-15 17:11:34 -0800

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,69 +1,90 @@
 #!/usr/bin/python3
-""" Module Rectangle"""
+'''
+    Class Rectangle
+'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ Class Rectangle """
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
-    def integer_validator(self, name, value):
-        """
-        Validates an integer
-        """
-        if type(value) is not int:
+    @property
+    def width(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
+
+    @staticmethod
+    def setter_validation(name, value):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if name == "x" or name == "y":
             if value < 0:
                 raise ValueError("{} must be >= 0".format(name))
         elif value <= 0:
             raise ValueError("{} must be > 0".format(name))
-
-    @property
-    def width(self):
-        """ getter width """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ setter width """
-        self.integer_validator(width, value)
-        self.__width = value
-
-    @property
-    def height(self):
-        """ getter heigth """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ setter height """
-        self.integer_validator(height, value)
-        self.__height = value
-
-    @property
-    def x(self):
-        """ getter x """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """ setter """
-        self.integer_validator(x, value)
-        self.__x = value
-
-    @property
-    def y(self):
-        """ getter y """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """ setter y """
-        self.integer_validator(y, value)
-        self.__y = value
