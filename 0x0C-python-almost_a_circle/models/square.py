1e8c8a75e0a10cdcfb651eb8fe00810c3bece9d4
Saeed-hasan <saeedmohammed1999@gmail.com>
2024-01-17 15:41:56 -0800

    all

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,18 @@ class Square(Rectangle):
     '''
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ main class """
+         '''
+        Constructor for initializing instance values.
+
+        Args:
+            size (int): The length of each side of the square.
+            x (int): The x-coordinate of the square's position (default 0).
+            y (int): The y-coordinate of the square's position (default 0).
+            id (int): The ID of an instance of this class (default None).
+
+        Returns:
+            None.
+        '''
         super().__init__(size, size, x, y, id)
         self.size = size
 
@@ -34,3 +45,28 @@ class Square(Rectangle):
         """ setter """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ update square by new value """
+        if args is not None and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+            else kwargs is not None and len(kwargs) != 0:
+                for (key, value) in kwargs.items():
+                    if key == "id":
+                        self.id = value
+                    elif key == "size":
+                        self.size = value
+                    elif key == "x":
+                        self.x = value
+                    elif key == "y":
+                        self.y = value
