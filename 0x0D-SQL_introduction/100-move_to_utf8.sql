e19a2f55be70316f98ac087951405948c10d5a59
Saeed-hasan <saeedmohammed1999@gmail.com>
2024-01-16 16:12:21 -0800

    all

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,6 @@
--- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) 
--- converts hbtn_0c_0 database to UTF8
-USE hbtn_0c_0;
+-- Change collation in database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER DATABASE hbtn_0c_0
-COLLATE utf8mb4_unicode_ci;
+use hbtn_0c_0;
 
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
