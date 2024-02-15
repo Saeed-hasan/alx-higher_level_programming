7e128e949ce7ccef56dcada9cb97973781fe3542
Saeed-hasan <saeedmohammed1999@gmail.com>
2024-02-14 18:06:59 -0800

    all

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file1 = fs.readFileSync(process.argv[2], 'utf-8');
+const file2 = fs.readFileSync(process.argv[3], 'utf-8');
+
+const file3 = (file1 + '' + file2);
+fs.writeFile(process.argv[4], file3, 'utf-8', err => {
+  if (err) throw err;
+});
